969b454e1b9d670867905547ec10c906a664b161
Wayout-pan <52097398+Wayout-pan@users.noreply.github.com>
2023-04-29 14:57:49 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'ouFBA5n24j21uAuJab1xZjnnQcrM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '6uoHl7SToDZ8_zht_jUSpi1o5sHqq1DzfXNOUce8kj4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
