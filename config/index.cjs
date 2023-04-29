b33cf49a23d7168542d322ac21e2121f685c8048
Wayout-pan <52097398+Wayout-pan@users.noreply.github.com>
2023-04-29 15:03:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -14,7 +14,7 @@ const USER_CONFIG = {
   APP_SECRET: 'b66dc51d1431fa376084c95583d8cd23',
 
   PROVINCE: '重庆',
-  CITY: '大学城',
+  CITY: '重庆-大学城',
 
   USERS: [
     {
@@ -32,19 +32,19 @@ const USER_CONFIG = {
           type: '*生日', name: '宝贝', year: '2001', date: '03-07',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        //{
+          //type: '生日', name: '李四', year: '1996', date: '09-31',
+        //},
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2021', date: '01-13',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2021-05-20' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        //{ keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
